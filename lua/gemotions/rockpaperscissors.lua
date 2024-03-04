c0262765cb7d3f7eccccc6a0083e7d0de439f2a8
Livelandr <Livelandr@gmail.com>
2024-03-04 19:52:04 +0300

    first commit

---
+++ lua/gemotions/rockpaperscissors.lua
@@ -0,0 +1,3 @@
+gemotions.RegisterEmote("rockpaperscissors", "gemotions/rockpaperscissors/paper.png", "gemotions/rockpaperscissors/paper.ogg")
+gemotions.RegisterEmote("rockpaperscissors", "gemotions/rockpaperscissors/rock.png", "gemotions/rockpaperscissors/rock.ogg")
+gemotions.RegisterEmote("rockpaperscissors", "gemotions/rockpaperscissors/scissors.png", "gemotions/rockpaperscissors/scissors.ogg")
